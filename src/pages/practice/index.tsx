a512283a99c739a05cac4089550791375febe25c
INSERT-io <isomanatsuki@team-lab.com>
2025-03-31 13:23:28 +0900

    initial commit

--- src/pages/practice/index.tsx
+++ src/pages/practice/index.tsx
@@ -1,18 +1,29 @@
 import * as THREE from "three";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, Vector3 } from "@react-three/fiber";
 import { useRef, useState } from "react";
+import { OrbitControls } from "@react-three/drei";
 
 const Page = () => {
   return (
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box />
+      <Box position={0} size={[0.1, 0.1, 0.1]} />
+      <Box position={1} size={[1, 1, 1]} />
+      <Box position={2} size={[0.5, 1, 1]} />
+      <OrbitControls />
+      <directionalLight position={[1, 1, 1]} intensity={0.5} />
     </Canvas>
   );
 };
 
-const Box = () => {
+// Props にnumberの配列を受け取る
+type BoxProps = {
+  position: Vector3;
+  size: [number, number, number];
+};
+
+const Box = ({ position, size }: BoxProps) => {
   const ref = useRef<THREE.Mesh>(null!);
   const [count, setCount] = useState(0);
 
@@ -26,9 +37,10 @@ const Box = () => {
       onClick={() => {
         setCount(() => (count + 32) % 255);
       }}
+      position={position}
     >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshBasicMaterial color={`rgb(0,${count},255)`} />
+      <boxGeometry args={size} />
+      <meshPhongMaterial color={`rgb(0,${count},255)`} />
     </mesh>
   );
 };
